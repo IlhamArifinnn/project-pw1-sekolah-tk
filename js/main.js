05afa41d805da69253a5c24ebec69e50c13ad826
IlhamArifinnn <arifinn2208@gmail.com>
2023-12-10 07:18:09 +0700

    ubah isi navbar

--- js/main.js
+++ js/main.js
@@ -28,14 +28,14 @@
     
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
+        if ($(this).scrollTop() > 600) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 'easeInOutExpo');
         return false;
     });
 
@@ -79,3 +79,41 @@
     
 })(jQuery);
 
+
+
+// jam digital
+function jam() {
+    setInterval(function () {
+ 
+       var waktu = new Date();
+       var jam   = document.getElementById('clock');
+       var hours = waktu.getHours();
+       var minutes = waktu.getMinutes();
+       var seconds = waktu.getSeconds();
+ 
+       if (waktu.getHours() < 10)
+       {
+          hours = '0' + waktu.getHours();
+       }
+       if (waktu.getMinutes() < 10)
+       {
+          minutes = '0' + waktu.getMinutes();
+       }
+       if (waktu.getSeconds() < 10)
+       {
+          seconds = '0' + waktu.getSeconds();
+       }
+       jam.innerHTML  = '<span>' +  hours + ":" + '</span>'
+                      + '<span>' + minutes + '</span>'
+                      + ":" + '<span>' + seconds +'</span>';
+ 
+       var spans      = jam.getElementsByTagName('span');
+       animation(spans[2]);
+       if (seconds == 0) animation(spans[1]);
+       if (minutes == 0 && seconds == 0) animation(spans[0]);
+ 
+    }, 1000);
+ }
+ 
+ jam();
+ 
